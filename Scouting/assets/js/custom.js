3b06cacf0c700c44e168da85c2aed4c0246ee662
Michael Ray <mray190@gmail.com>
2018-02-28 15:07:42 -0600

    Added the data tab

--- Scouting/assets/js/custom.js
+++ Scouting/assets/js/custom.js
@@ -37,3 +37,34 @@ var config = {
 	databaseURL: "https://viperbotsvalor.firebaseio.com",
 	storageBucket: "bucket.appspot.com"
 };
+
+var tba_api_key = "LCBZ7qqYrBR0e06C4QJEjaW1O7r2TZat7KZwvQcfDqShwIxV4N7epHK9lbafjc4M";
+var tba_base_url = "https://www.thebluealliance.com/api/v3/";
+
+function tbaCall(url_extension, last_modified, callback) {
+	$.ajax({
+		url: tba_base_url+url_extension,
+		type: "GET",
+ 		headers: {
+    		// "if-modified-since": last_modified,
+ 			'x-tba-auth-key': tba_api_key
+ 		},
+ 		success: callback
+	});
+}
+
+function saveToStorage(key, data, last_modified) {
+	if (typeof(Storage) !== "undefined") {
+		localStorage.setItem(key, JSON.stringify(data));
+		setCookie("lastModified_"+key, last_modified, 1);
+	}
+}
+
+function getData(key, url_extension, callback) {
+	tbaCall(url_extension, getCookie("lastModified_" + key), function(data) {
+		var last_modified = "";
+		var old_data = localStorage.getItem(key);
+		saveToStorage(key, data, last_modified);
+		callback(old_data, data);
+	});
+}
