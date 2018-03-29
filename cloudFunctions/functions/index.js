c1ec40f33bcab4fe9951576af3ab6df7a368d1b9
AdamBusch <adamjbusch561@gmail.com>
2018-03-29 09:31:24 -0500

    Added force team update

--- cloudFunctions/functions/index.js
+++ cloudFunctions/functions/index.js
@@ -3,18 +3,31 @@ const functions = require('firebase-functions');
 // https://firebase.google.com/docs/functions/database-events
 
 exports.calculateAvgs = functions.database.ref('/{regional_code}/teams/{team_num}/matches').onWrite((event) => {
-  console.log('Event Type : '+ event.eventType);
   if(event.eventType === 'providers/google.firebase.database/eventTypes/ref.delete') return;
-
 
   const original = event.data;
   const regional_code = event.params.regional_code;
   const team_num = event.params.team_num;
 
-	var rootRef = original.ref.root;
-  var team_average_ref = rootRef.child('/' + regional_code + '/teams/' + team_num + '/averages');
-  var raw_results_ref = rootRef.child('/' + regional_code + '/raw_results');
-  var team_hash_list =  rootRef.child('/' + regional_code + '/teams/' + team_num + '/matches');
+  return calculateTeam(regional_code, team_num);
+});
+
+exports.calculateTeam = functions.https.onCall((data, context) => {
+  const regional_code = data.regional_code;
+  const team_num = data.team_num;
+  
+  console.log('Requested to calculate ' + regional_code + '/' + team_num);
+  return calculateTeam(regional_code, team_num);
+});
+
+
+
+
+function calculateTeam(regional_code, team_num) {
+
+  var team_average_ref = functions.database.ref('/' + regional_code + '/teams/' + team_num + '/averages');
+  var raw_results_ref = functions.database.ref('/' + regional_code + '/raw_results');
+  var team_hash_list = functions.database.ref('/' + regional_code + '/teams/' + team_num + '/matches');
   // Properties that don't get averaged because that would be bad...
   const dont_average = ['auto_start', 'alliance', 'match_num', 'team_num'];
 
@@ -34,6 +47,11 @@ exports.calculateAvgs = functions.database.ref('/{regional_code}/teams/{team_num
         total_num_matches ++;
         const hash = hash_snapshot.val();
         const match = raw_results[hash];
+
+        if(!match) {
+          console.error('Unable to find match hash: ' + hash + ' for team: ' + team_num);
+          return;
+        }
   
         switch(match['auto_start'].toLowerCase()) {
           case('left') : {averages.left_start = (averages.left_start || 0) + 1; break;}
@@ -64,8 +82,14 @@ exports.calculateAvgs = functions.database.ref('/{regional_code}/teams/{team_num
           // Average all other props by their match data totals
           else { 
             averages[prop] = (averages[prop] || 0.0) + parseFloat(match[prop]);
-            if(averages[prop + '_max'] < match[prop]) {
-              averages[prop + '_max'] = match[prop];
+            if(averages[prop + '_max'] < match[prop] || !(averages[prop + '_max'])) {
+              averages[prop + '_max'] = parseFloat(match[prop]);
+            }
+
+            if(prop === 'hang_time' && match['hang_attempt']) {
+              if((match[prop] < averages[prop + '_min'] && match[prop] > 0) || !(averages[prop + '_min'])) {
+                averages[prop + '_min'] = parseFloat(match[prop]);
+              }
             }
           }
         }
@@ -75,13 +99,13 @@ exports.calculateAvgs = functions.database.ref('/{regional_code}/teams/{team_num
       const total_hang_attempts = (averages['hang_attempt'] > 0) ? averages['hang_attempt'] : 1;
 
       for(i in averages) {
-        if(i.toLowerCase() === 'comments' || i.includes('_max')) continue;
+        if(i.toLowerCase() === 'comments' || i.includes('_max') || i.includes('_min')) continue;
 
           // Only average hang attempts based on how many times they attempt
         if(i.toLowerCase() === 'hang_succeed' || i.toLowerCase() === 'host_succeed' || i.toLowerCase() === 'hang_time') {
-          averages[i] = (parseFloat(averages[i]) / total_hang_attempts).toFixed(5);
+          averages[i] = parseFloat((parseFloat(averages[i]) / total_hang_attempts).toFixed(5));
         } else {
-          averages[i] = (parseFloat(averages[i]) / total_num_matches).toFixed(5);
+          averages[i] = parseFloat((parseFloat(averages[i]) / total_num_matches).toFixed(5));
         }
       }
 
@@ -89,4 +113,4 @@ exports.calculateAvgs = functions.database.ref('/{regional_code}/teams/{team_num
       return team_average_ref.set(averages);
     });
   })
-});
+}
