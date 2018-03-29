b221e2e364fa85b769669a54e28660fb3139a9c5
AdamBusch <adamjbusch561@gmail.com>
2018-03-28 23:55:19 -0500

    Fixed host hang average and display

--- cloudFunctions/functions/index.js
+++ cloudFunctions/functions/index.js
@@ -3,6 +3,9 @@ const functions = require('firebase-functions');
 // https://firebase.google.com/docs/functions/database-events
 
 exports.calculateAvgs = functions.database.ref('/{regional_code}/teams/{team_num}/matches').onWrite((event) => {
+  console.log('Event Type : '+ event.eventType);
+  if(event.eventType === 'providers/google.firebase.database/eventTypes/ref.delete') return;
+
 
   const original = event.data;
   const regional_code = event.params.regional_code;
@@ -13,7 +16,7 @@ exports.calculateAvgs = functions.database.ref('/{regional_code}/teams/{team_num
   var raw_results_ref = rootRef.child('/' + regional_code + '/raw_results');
   var team_hash_list =  rootRef.child('/' + regional_code + '/teams/' + team_num + '/matches');
   // Properties that don't get averaged because that would be bad...
-  const dont_average = ['auto_start', 'alliance', 'match_num'];
+  const dont_average = ['auto_start', 'alliance', 'match_num', 'team_num'];
 
   return raw_results_ref.once('value' , (raw_results_snapshot) => {
     const raw_results = raw_results_snapshot.val();
@@ -54,6 +57,8 @@ exports.calculateAvgs = functions.database.ref('/{regional_code}/teams/{team_num
           else if(typeof(match[prop]) === "boolean") { 
            if(match[prop]) {
              averages[prop] = (averages[prop] || 0.0) + 1.0;
+           } else {
+            averages[prop] = (averages[prop] || 0.0);
            }
           }
           // Average all other props by their match data totals
